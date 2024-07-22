12d1b9fdf28c9fa04a78901749ce7d34b232b0c3
habiba zguaid <zguaidhabiba@gmail.com>
2024-07-22 23:40:10 +0100

    task 8

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,7 +4,7 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return{
+  return {
   [`income-${getCurrentYear()}`] = income;
   [`gdp-${getCurrentYear()}`] = gdp;
   [`capita-${getCurrentYear()}`] = capita;
