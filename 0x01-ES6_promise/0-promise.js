cdab3fcd38a948d28d9cd2a426cabf4d385950d2
habiba zguaid <zguaidhabiba@gmail.com>
2024-07-24 19:19:59 +0100

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,5 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-      resolve()
-    }
-    else {
-      reject()
-    }
+    resolve()
   });
 }
