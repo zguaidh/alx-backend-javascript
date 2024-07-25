0f1e8930b87846175da0d86370499e6c3ed9bf1d
habiba zguaid <zguaidhabiba@gmail.com>
2024-07-25 22:39:48 +0100

    task2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Each student must be a string');
+      }
+    }
+    this._students = students;
+  }
+}
