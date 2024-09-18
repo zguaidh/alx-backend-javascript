fb10ca0d3d3a2d0ca054652f97161675f9a95904
zguaidh <zguaidhabiba@gmail.com>
2024-09-18 21:11:43 +0000

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+
+async function countStudents(file) {
+  try {
+    const data = await fs.readFile(file, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const csStudents = [];
+    const sweStudents = [];
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const fields = lines[i].split(',').map((field) => field.trim());
+
+      const firstname = fields[0];
+      const field = fields[3];
+
+      if (firstname && field) {
+        if (field === 'CS') {
+          csStudents.push(firstname);
+        } else if (field === 'SWE') {
+          sweStudents.push(firstname);
+        }
+      }
+    }
+
+    const totalStudents = csStudents.length + sweStudents.length;
+
+    console.log(`Number of students: ${totalStudents}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
