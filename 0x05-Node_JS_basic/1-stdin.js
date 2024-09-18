0d97acb7af0fb6741adf9f05c6fd2361e6d4db94
zguaidh <zguaidhabiba@gmail.com>
2024-09-18 21:03:15 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,9 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
